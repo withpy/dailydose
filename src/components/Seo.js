39007089e9b62acf6d53b91410d5d409f9d73974
Mohit Kumar <mr.randomthing@gmail.com>
2024-01-10 21:19:29 +0530

    added seo tags

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import preview from "../media/dailydose_preview.png"
 export default function Seo() {
   return (
+    <HelmetProvider >
     <Helmet defer={false}>
       <meta property="og:title" content="DailyDose: Your Ultimate Real-Time News Hub with Dynamic Categories and Powerful Search Features" />
       <meta property="og:image" content={window.location.origin+preview} />
@@ -11,6 +12,6 @@ export default function Seo() {
         content="Stay informed with DailyDose, your daily source for breaking news, trending stories, and in-depth analysis. Visit our news app for the latest updates on politics, technology, entertainment, and more."
       />
       <meta property="og:url" content={window.location.href} />
-    </Helmet>
+    </Helmet></HelmetProvider>
   );
 }
